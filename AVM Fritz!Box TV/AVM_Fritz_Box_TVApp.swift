b0fce7f302b08d18e6b9d11c2a5efb3f1471d289
Roman Roibu <roman@roibu.xyz>
2024-06-25 18:16:56 +0200

    Initial Commit

---
+++ AVM Fritz!Box TV/AVM_Fritz_Box_TVApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVM_Fritz_Box_TVApp.swift
+//  AVM Fritz!Box TV
+//
+//  Created by Roman Roibu on 25.06.24.
+//
+
+import SwiftUI
+
+@main
+struct AVM_Fritz_Box_TVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
