7700720d058d77e1412634a2f3551cbeaf7c385a
Roman Roibu <roman@roibu.xyz>
2024-06-26 02:32:38 +0200

    Update app to show tabs of channel collections

--- AVM Fritz!Box TV/Application.swift
+++ AVM Fritz!Box TV/Application.swift
@@ -1,10 +1,40 @@
 import SwiftUI
 
+struct TabItemView: View {
+    let type: ChannelType
+
+    var body: some View {
+        switch type {
+        case .sd: return Text("SD")
+        case .hd: return Text("HD")
+        case .radio: return Text("Radio")
+        }
+    }
+}
+
 @main
-struct AVM_Fritz_Box_TVApp: App {
+struct Application: SwiftUI.App {
+    @State private var sd = [Channel]()
+    @State private var hd = [Channel]()
+    @State private var radio = [Channel]()
+
+    let discovery = Discovery()
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                TabView {
+                    CollectionView(channels: $sd)
+                        .tabItem { TabItemView(type: .sd) }
+                        .task { $sd.wrappedValue = try! await discovery.fetchChannels(type: .sd) }
+                    CollectionView(channels: $hd)
+                        .tabItem { TabItemView(type: .hd) }
+                        .task { $hd.wrappedValue = try! await discovery.fetchChannels(type: .hd) }
+                    CollectionView(channels: $radio)
+                        .tabItem { TabItemView(type: .radio) }
+                        .task { $radio.wrappedValue = try! await discovery.fetchChannels(type: .radio) }
+                }
+            }
         }
     }
 }
